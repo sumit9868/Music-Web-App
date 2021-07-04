d883d56b768f073bdc5c4b566bd5ac8451b988a9
Prabhjot Singh <prabhjotlamba2001@gmail.com>
2021-07-05 01:55:30 +0530

    extracted token from API

--- src/JS/App.js
+++ src/JS/App.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../CSS/App.css';
-import Login from  './Login.js';
+import Login from './Login.js';
+import { getTokenFromUrl } from "../API/spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    //Run code based on a given condition {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("This is a user", user);
+      });
+    }
+    console.log("We got a token", _token);
+  }, []);
+
   return (
     <div className="App">
-      {/* <h1>HELLO</h1> */}
-     
-      {/* Spotify Logo */}
-      <Login/>
-      {/*login with spotify logo*/}
-      
+
+      {
+        token ? (
+
+          <h1>stfu chamAN SUMIT</h1>
+        ) : (
+          <Login />
+        )
+      }
+
     </div>
   );
 }
